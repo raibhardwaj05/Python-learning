8a6c2ddd89b912fdbbd3a08925e16a7ff757cca7
raibhardwaj05 <bhardwajsheojeerai@gmail.com>
2025-05-28 15:55:05 +0530

    Add files via upload

---
+++ Day2/DataTypes/Subscripting.py
@@ -0,0 +1,11 @@
+# subscripting.
+
+# It means using square brackets [] with an index number to get a specific character from the string.
+print("Hello"[0])
+# Calling or accessing any element from its location (index) in a string is called subscripting.
+
+# 0 to (n-1) ==>> checks from beginning i.e from H in "hello" ===>>> asending order
+# -1 to -n ==>> checks from ending i.e from o in "hello" ===>>> desending order
+
+x = int(input("Enter the position : "))
+print("Hello"[x])
---
+++ Day2/DataTypes/Types.py
@@ -0,0 +1,16 @@
+# Strings ==>> anything in ("_") is a string
+print("Count" + " 1234") # (+) ==>> concatination (addition of strings)
+
+# Integer ==>> whole numbers (no decimal values)
+print(5678)
+print(12 + 12) # this is adding of numbers (not concatination)
+
+# Large integers
+print(256_456_785) # compiler will understand it is a large number and will ignore (_) while compiling
+# this is just for presentation not necesscity
+
+# Float ==>> decimal values
+print(3.14)
+
+# Boolean ==>> true or false
+print(True)
