313c1a55f8e2aea5f145189272cbb50a75fd4ca4
raibhardwaj05 <bhardwajsheojeerai@gmail.com>
2025-05-28 13:15:44 +0530

    Add files via upload

---
+++ Day/Input/input.py
@@ -0,0 +1,12 @@
+# input data
+
+# Syntax ===>>> input("messege")
+# ye messege output karke input lega
+
+input("Enter your name ?") # equals to x
+
+print("Hello " + input("Your name ?") + "!")
+
+# print("message" + input(ye x ke equal nahi hai alag function hai))
+# 1. Runs ==>> input("Your name ?")
+# 2. Runs ==>> print("Hello" + "jo message input hua")
