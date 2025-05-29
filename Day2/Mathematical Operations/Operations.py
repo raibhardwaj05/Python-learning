0489c4b790ea3d560aa1358886068c8f8428f6e7
raibhardwaj05 <bhardwajsheojeerai@gmail.com>
2025-05-29 09:05:50 +0530

    Add files via upload

---
+++ Day2/Mathematical Operations/Operations.py
@@ -0,0 +1,34 @@
+# Operators
+
+# Addition
+print(6+3) # == 9
+
+# Subtraction
+print(6-3) # == 3
+print(3-6) # == -3
+
+# Multiplication
+print(2*3) # == 6
+
+# Power 2 cube, square and all types ===>>> (number ** power) ==>> Syntax
+print(2 ** 3) # == 8
+
+# Division with floating Values
+print(6/3) # == 2.0
+print(3/6) # == 0.5
+
+# Division without Floating values ==>> returns integer
+print(3 // 6) # == 0
+print(type(3 // 6)) # ==>> <class = 'int'>
+
+# rules for calculation ==>> PEMDAS
+# Parenthesis ==> ()
+# Exponents / Powers ==> **
+
+# Multiplication / Division ==> * OR /
+# Addition / Subtraction ==> + OR -
+# Left most will be operated first ==>> 3*6/2 ==>> Mul then Div ==>> 3+6-2 ==>> Add then Sub
+
+print(3*6/2) # == 9.0
+print(3+6-2) # == 7
+print(3 * (3 + 3) / 3 - 3) # == 3
