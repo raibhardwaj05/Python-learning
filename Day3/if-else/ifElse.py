e55b876e7a75b28a3f86c9eb44268ec6f20a29e5
raibhardwaj05 <bhardwajsheojeerai@gmail.com>
2025-05-31 15:25:04 +0530

    Add files via upload

---
+++ Day3/if-else/ifElse.py
@@ -0,0 +1,11 @@
+# if condition:
+#   do this
+# else:
+#   do this
+
+a = int(input("Enter the water level: "))
+
+if a >= 80:
+    print("Drain water")
+else:
+    print("continue filling")
---
+++ Day3/if-else/ticketCondition.py
@@ -0,0 +1,9 @@
+# if height is more than 120cm can get the ticket for the ride
+
+print("Welcome to the rollercoaster!")
+height = int(input("Enter your height in cm: "))
+
+if height > 120:
+    print("You can ride")
+else:
+    print("You cannot ride")
