2758a2fb250cf087f0a1fb0c5dd86744571a0ae1
raibhardwaj05 <bhardwajsheojeerai@gmail.com>
2025-06-14 00:49:18 +0530

    Add files via upload

---
+++ Day5/For Loops/For.py
@@ -0,0 +1,11 @@
+# for loop
+
+fruits = ["apple", "peach", "banana"]
+
+for fruit in fruits:
+    print(fruit)
+
+nth_number = int(input("How many numbers you want to print: "))
+# to print natural numbers
+for num in range(5, nth_number+1): # range(start, end)
+    print(num)
