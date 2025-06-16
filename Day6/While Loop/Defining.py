da63c43f148c1c35754036f3c148341f493e46f0
raibhardwaj05 <bhardwajsheojeerai@gmail.com>
2025-06-16 22:52:31 +0530

    Add files via upload

---
+++ Day6/While Loop/Defining.py
@@ -0,0 +1,9 @@
+natural_number = int(input("How many natural numbers you want to print? : "))
+num = 1
+
+while natural_number > 0:
+    print(num)
+    num += 1 # incrementing the number to be printed
+    natural_number -= 1 # decrementing the number of counts
+
+
